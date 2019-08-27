f1c7e0e771b9969b2800884d0a074a0d062dbcfe
Philip Haupt <philiphaupt@users.noreply.github.com>
2019-08-27 15:59:57 +0100

    update add code table by the window
    here goes

--- coffee.R
+++ coffee.R
@@ -28,6 +28,8 @@ coffee_df = tibble::tibble(
   hometown
 )
 
+
+
 coordinates = tmaptools::geocode_OSM(coffee_df$hometown)
 
 coffee_df = tibble::tibble(
@@ -48,9 +50,17 @@ tm_shape(coffee_sf) + tm_dots()
 
 write.csv(coffee_df, "coffee.csv")
 
-#added data from back table
-name=c("Izzie","Greg", "Katherine", "Jeremy", "Kara")
-coffee=c(2,3,5,2,0)
-town=c("High Wickham","Budapest","Bristol","Chester","Buffalo")
-new_coffee=data.frame(name,coffee,town)
-new_coffee
+#---------------------table by the window----------------
+# recreation example
+person_name <-
+        c("olivia", "daniel", "phil", "claudia") # this is a vector (a series of vectors of the same class)
+
+n_coffee <- c(7, 3, 10, 14)
+
+hometown <- c("doncaster", "harlech", "pretoria", "mexico_city")
+
+coffee_df_tbtw <- data.frame(person_name,
+              n_coffee,
+              hometown
+              )
+#---------------------------------------------------------
